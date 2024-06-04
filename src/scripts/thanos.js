ffc9b5406f24a7127a838cf5f5c926377510b34d
MykhailoVobolis <mvobolis@gmail.com>
2024-06-04 23:47:19 +0300

    replace if on the filter

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -3,19 +3,15 @@ import { PATH_DB } from '../constants/contacts.js';
 
 // Видалення контакту з файлу db.json із вірогідністю 50 відсотків.
 export const thanos = async () => {
+  let arrayContacts;
   try {
     const curentContacts = await fs.readFile(PATH_DB, 'utf8');
-    const arrayContacts = JSON.parse(curentContacts);
-    let randomArray = [];
+    arrayContacts = JSON.parse(curentContacts);
 
     // Рандомна умова 50/50
-    arrayContacts.map((i) => {
-      const random = Math.random();
-      if (random > 0.5) {
-        randomArray.push(i);
-      }
-    });
-    await fs.writeFile(PATH_DB, JSON.stringify(randomArray), 'utf8');
+    arrayContacts = arrayContacts.filter(() => Math.random() > 0.5);
+
+    await fs.writeFile(PATH_DB, JSON.stringify(arrayContacts), 'utf8');
   } catch (err) {
     console.error('Помилка:', err);
   }
